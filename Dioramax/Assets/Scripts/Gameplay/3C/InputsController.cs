230a96c11a0df5d668dafc22651f5ac840746114
Benji <f.nossin@rubika-edu.com>
2022-02-02 17:15:23 +0100

    resizing viewport

--- Dioramax/Assets/Scripts/Gameplay/3C/InputsController.cs
+++ Dioramax/Assets/Scripts/Gameplay/3C/InputsController.cs
@@ -1,18 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class InputsController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private LayerMask cubeMask;
+    [SerializeField] private Camera camera;
+    [SerializeField] private PlaceholderFeedback placeholderFeedback;
 
-    // Update is called once per frame
-    void Update()
+    private Vector3 origin;
+    private Vector3 end;
+    private Touch touch; 
+    // Input.touches to track multiple fingers -> zoom
+    
+    public void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            origin = transform.position; 
+            end = camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 30f));
+            Debug.DrawRay(origin, (end-origin) * 100f, Color.red, 0.5f); 
+            
+            if (Physics.Raycast(origin, (end-origin), out RaycastHit hitInfo, 100f, cubeMask))
+            {
+                Debug.DrawRay(origin, (end-origin), Color.green, 0.5f); 
+                placeholderFeedback.ChangeColor();
+            }
+        }
     }
 }
