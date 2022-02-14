60bc3c3ed911e037ea2f2b24f8358070bdcdc8e3
Benji <f.nossin@rubika-edu.com>
2022-02-14 01:47:13 +0100

    rotation rework #4 (crane object for camera)

--- Dioramax/Assets/Scripts/Gameplay/3C/CameraRotation.cs
+++ Dioramax/Assets/Scripts/Gameplay/3C/CameraRotation.cs
@@ -3,54 +3,37 @@ using UnityEngine;
 
 public class CameraRotation : MonoBehaviour
 {
-    [SerializeField] private GameObject diorama; 
+    [SerializeField] private GameObject diorama;
+
     [SerializeField, Range(0.2f, 5f)] private float rotationForce = 2f;
     [SerializeField, Range(0, 50)] private float rotationSensitivity = 5f;
 
-    private Vector2 swipeDirection; 
     private Camera mainCam;
     private Vector2 rotationAxis;
 
     private void Start()
     {
-        mainCam = Camera.main; 
-    }
-
-    private void Update()
-    {
-        // Debug.Log($"sin value is " + MathF.Sin(Vector2.Angle(Vector3.down, swipeDirection)));
-        // Debug.Log($"Cos value is " + MathF.Cos(Vector2.Angle(Vector3.down, swipeDirection)));
-
-        // if swipe direction = 0,1, sin should be -1
-        // generated 90° angle should be -1,0 (by multiplying x by sign of sin
-
-
-        // swipe right 1,0,0
-        // mainCam.transform.RotateAround(diorama.transform.position, Vector3.up, Time.deltaTime * -sensitivity * Mathf.Sign(Vector3.right.x));
-
-        // swipe up 0,1,0
-        // mainCam.transform.RotateAround(diorama.transform.position, Vector3.right, Time.deltaTime * sensitivity * Mathf.Sign(Vector3.up.x));
-
-        // swipe diag top/right 1,1,0 
-        // -1,1,0
-        // mainCam.transform.RotateAround(diorama.transform.position, new Vector3(-1,1,0), Time.deltaTime * -sensitivity * Mathf.Sign(1));
-
-        // swipe diag bot/left -1,1,0
-        // 1,1,0
-        // mainCam.transform.RotateAround(diorama.transform.position, new Vector3(1, 1, 0), Time.deltaTime * sensitivity * Mathf.Sign(-1));
+        mainCam = Camera.main;
+        transform.position = diorama.transform.position; // to set dynamically at the start of a level
     }
 
     public void UpdateRotation(Vector3 rotationDirection, float rotationForce)
     {
-        if (rotationForce >= rotationSensitivity)
+        // grand-parent only Y axis
+        // parent only X axis
+        // camera does not rotate
+
+        if (rotationForce >= rotationSensitivity) 
         {
             // to always get an axis that is 90° more than direction
-            rotationAxis = new Vector2(-rotationDirection.y, rotationDirection.x);
+            rotationAxis = new Vector2(-rotationDirection.y, rotationDirection.x); // -y
 
-            mainCam.transform.RotateAround(
+            /* transform.RotateAround(
                 diorama.transform.position,
                 rotationAxis,
-                Time.deltaTime * this.rotationForce * rotationForce);
+                Time.deltaTime * this.rotationForce * rotationForce); */
+
+            transform.Rotate(rotationAxis, Time.deltaTime * this.rotationForce * rotationForce); 
         }
     }
 
