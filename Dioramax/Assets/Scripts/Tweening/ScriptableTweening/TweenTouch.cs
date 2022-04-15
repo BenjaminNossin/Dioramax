ea1470d331467a06afbcfbe2824c53d2dfeef7cd
Lucrezia <lucrezia.romeo@gmail.com>
2022-04-16 01:53:19 +0200

    tween on touch

--- Dioramax/Assets/Scripts/Tweening/ScriptableTweening/TweenTouch.cs
+++ Dioramax/Assets/Scripts/Tweening/ScriptableTweening/TweenTouch.cs
@@ -9,25 +9,25 @@ public class TweenTouch : MonoBehaviour
 
     // TempPosition
     private Vector3 ObjectJumpPosition;
-    void Start()
+    public void Tween()
     {
 
         // ! remove looping
         ObjectJumpPosition = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + td.up_max_position, gameObject.transform.position.z);
 
          //bounce
-        LeanTween.moveLocal(gameObject, ObjectJumpPosition, td.time_bounce).setEasePunch().setLoopCount(-1);
+        LeanTween.moveLocal(gameObject, ObjectJumpPosition, td.time_bounce).setEasePunch();
 
         //stretch&squash
-        LeanTween.scale(gameObject, td.stretch_squash, 1f).setEasePunch().setLoopCount(-1);
+        LeanTween.scale(gameObject, td.stretch_squash, 1f).setEasePunch();
 
         //rotation
 
         if (td.EaseOutCubic)
-            LeanTween.rotateAround(gameObject, td.RotationAxis, td.rotation_degrees, td.time_rotation).setEaseOutCubic().setLoopCount(-1);
+            LeanTween.rotateAround(gameObject, td.RotationAxis, td.rotation_degrees, td.time_rotation).setEaseOutCubic();
 
         if (td.Punch)
-            LeanTween.rotateAround(gameObject, td.RotationAxis, td.rotation_degrees, td.time_rotation).setEasePunch().setLoopCount(-1);
+            LeanTween.rotateAround(gameObject, td.RotationAxis, td.rotation_degrees, td.time_rotation).setEasePunch();
 
 
     }
