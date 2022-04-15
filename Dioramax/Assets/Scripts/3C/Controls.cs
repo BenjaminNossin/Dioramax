6e10aad98375687358a1439419679625aebae4c6
Benji <f.nossin@rubika-edu.com>
2022-04-15 18:20:39 +0200

    framerate fix #1

--- Dioramax/Assets/Scripts/3C/Controls.cs
+++ Dioramax/Assets/Scripts/3C/Controls.cs
@@ -78,20 +78,20 @@ public class Controls : MonoBehaviour
     // You should better differentiate what is in Update, FixedUpdate and LateUpdate.. 
     private void Update()
     {
-        if (updateSeconds >= 1f)
+        /* if (updateSeconds >= 1f)
         {
             Debug.Log("mobile update fps : " + mobileUpdateFPSCounter);
             updateSeconds = 0f;
             mobileUpdateFPSCounter = 0; 
-        }
+        } 
 
         mobileUpdateFPSCounter++; 
-        updateSeconds += Time.deltaTime; 
+        updateSeconds += Time.deltaTime; */
     }
 
     private void FixedUpdate()
     {
-        if (fixedUpdateSeconds >= 1f)
+        /* if (fixedUpdateSeconds >= 1f)
         {
             Debug.Log("mobile fixedUpdate fps : " + mobileFixedUpdateFPSCounter);
             fixedUpdateSeconds = 0f;
@@ -99,7 +99,7 @@ public class Controls : MonoBehaviour
         }
 
         mobileFixedUpdateFPSCounter++;
-        fixedUpdateSeconds += Time.fixedDeltaTime;
+        fixedUpdateSeconds += Time.fixedDeltaTime; */
 
 
         // Do Once
