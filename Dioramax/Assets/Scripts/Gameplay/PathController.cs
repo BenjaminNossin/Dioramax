6efb0e7449517f9c86f88a0d1fe6c5296370fba5
Benji <f.nossin@rubika-edu.com>
2022-05-08 18:04:31 +0200

    path controller update (mostly QOL)

--- Dioramax/Assets/Scripts/Gameplay/PathController.cs
+++ Dioramax/Assets/Scripts/Gameplay/PathController.cs
@@ -1,19 +1,47 @@
 using UnityEngine;
 
 // stores all the node data and update path according to switches
+[ExecuteAlways]
 public class PathController : MonoBehaviour
 {
     public static PathController Instance;
-    [SerializeField] private PathNode[] Nodes; 
+    private PathNode[] Nodes; 
 
     private Vector3 currentIndexNodePosition;
 
     [Header("DEBUG")]
-    public bool doDebugDrawGizmos;
+    public bool refreshDrawGizmosOnNodePathChange;
+
+    private void OnValidate()
+    {
+    }
+
+    private void OnTransformChildrenChanged()
+    {
+        GameLogger.Log("Child count changed. Updating array"); 
+        Nodes = new PathNode[transform.childCount];
+
+        if (Nodes.Length != 0)
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                try
+                {
+                    Nodes[i] = transform.GetChild(i).GetComponent<PathNode>();
+                }
+                catch (System.Exception e)
+                {
+                    GameLogger.Log($"{e.Message}. \n"); 
+                    GameLogger.Log("A PathNode component has been added to the new object. \n If that object is not supposed to have that component, " +
+                        "please remove it from the PathController child hierarchy.");
+                }
+            }
+        }
+    }
 
     private void OnDrawGizmos()
     {
-        if (Nodes.Length != 0 && doDebugDrawGizmos)
+        if (Nodes.Length != 0 && refreshDrawGizmosOnNodePathChange)
         {
             for (var i = 0; i < Nodes.Length; i++)
             {
@@ -24,7 +52,15 @@ public class PathController : MonoBehaviour
 
                 for (int j = 0; j < Nodes[i].GetNextPossibleNodesArraySize(); j++)
                 {
-                    Gizmos.DrawLine(currentIndexNodePosition, Nodes[i].GetNextPossibleNodesTransform()[j].position);
+                    try
+                    {
+                        Gizmos.DrawLine(currentIndexNodePosition, Nodes[i].GetNextPossibleNodesTransform()[j].position);
+                    }
+                    catch (MissingReferenceException)
+                    {
+                        GameLogger.Log($"{Nodes[i].gameObject} is missing one or more references in its nextPossibleNodes array."); 
+                    }
+
                 }
             }
         } 
