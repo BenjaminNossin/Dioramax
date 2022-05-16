c2605676fee4a7044de1c3247f7292c31e43c76f
Benji <f.nossin@rubika-edu.com>
2022-05-16 15:17:28 +0200

    UI main menu

---
+++ Dioramax/Assets/Scripts/UI/CustomSceneManager.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class CustomSceneManager : MonoBehaviour
+{
+    public static CustomSceneManager Instance; 
+
+    [SerializeField] private bool isMainMenu;
+    [SerializeField] private DioramaName dioramaToLoad = DioramaName.Tutorial; // will be modified via save system with SetDioramaToLoad
+
+    private void Awake()
+    {
+        if (Instance)
+        {
+            Destroy(Instance);
+        }
+
+        Instance = this; 
+    }
+
+    private void Update()
+    {
+        // use a specific button if needed
+        if (isMainMenu && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            SceneManager.LoadSceneAsync((int)dioramaToLoad + 1, LoadSceneMode.Single); 
+            // have a fade out screen. When it's done, allowSceneActivation = true; 
+        }
+    }
+
+    public void LoadScene(int index)
+    {
+        if (index > SceneManager.sceneCountInBuildSettings - 1 || index < 0) return; 
+        SceneManager.LoadSceneAsync(index, LoadSceneMode.Single);
+    }
+
+    public void SetDioramaToLoad(DioramaName diorama)
+    {
+        dioramaToLoad = diorama; 
+    }
+
+    public void QuitApplication()
+    {
+        Application.Quit();
+    }
+}
