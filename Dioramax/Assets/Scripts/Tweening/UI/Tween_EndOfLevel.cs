79bcfde412f757eb36cb363be2e6bcfd246d1187
Lucrezia <lucrezia.romeo@gmail.com>
2022-05-23 19:27:24 +0200

    Tween End Of Level DONE

--- Dioramax/Assets/Scripts/Tweening/UI/Tween_EndOfLevel.cs
+++ Dioramax/Assets/Scripts/Tweening/UI/Tween_EndOfLevel.cs
@@ -8,11 +8,18 @@ public class Tween_EndOfLevel : MonoBehaviour
 
     public float stretch_squash;
     public float TimeScale;
-    
+
     // Start is called before the first frame update
     void Start()
     {
         transform.localScale = new Vector3(0, 0, 0);
-        transform.DOScale(stretch_squash, TimeScale).SetEase(Ease.OutBack).SetLoops(-1, LoopType.Restart) ;
+
     }
+
+        public void WinTween()
+        {
+
+       
+        transform.DOScale(stretch_squash, TimeScale).SetEase(Ease.OutBack);//.SetLoops(-1, LoopType.Restart) ;
+        }
 }
