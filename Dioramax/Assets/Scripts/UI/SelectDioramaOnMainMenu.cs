3b44a89d3a417043c2c44e6c803398388ca2a82a
Benji <f.nossin@rubika-edu.com>
2022-05-25 01:22:06 +0200

    intégration finale GA #3

--- Dioramax/Assets/Scripts/UI/SelectDioramaOnMainMenu.cs
+++ Dioramax/Assets/Scripts/UI/SelectDioramaOnMainMenu.cs
@@ -3,11 +3,15 @@ using UnityEngine;
 public class SelectDioramaOnMainMenu : MonoBehaviour
 {
     [SerializeField] private GameObject[] dioramas;
+    [SerializeField] private bool isMainMenu = true; 
     private int selector; 
 
     void Start()
     {
-        selector = Random.Range(0, 2);
-        dioramas[selector].SetActive(true);  
+        if (isMainMenu)
+        {
+            selector = Random.Range(0, 2);
+            dioramas[selector].SetActive(true);
+        }
     }
 }
